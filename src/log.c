29afaedad68e8a8e03e78090e7be078d7d213982
yanrbts <772166784@qq.com>
2024-06-04 18:20:27 +0800

    modify log.c

--- src/log.c
+++ src/log.c
@@ -54,14 +54,28 @@ static void stdout_callback(log_Event *ev) {
   char buf[16];
   buf[strftime(buf, sizeof(buf), "%H:%M:%S", ev->time)] = '\0';
 #ifdef LOG_USE_COLOR
+
+#if LOG_SHOW_LINE
   fprintf(
     ev->udata, "%s %s%-5s\x1b[0m \x1b[90m%s:%d:\x1b[0m ",
     buf, level_colors[ev->level], level_strings[ev->level],
     ev->file, ev->line);
 #else
   fprintf(
+    ev->udata, "%s %s%-5s\x1b[0m ",
+    buf, level_colors[ev->level], level_strings[ev->level]);
+#endif
+
+#else
+#if LOG_SHOW_LINE
+  fprintf(
     ev->udata, "%s %-5s %s:%d: ",
     buf, level_strings[ev->level], ev->file, ev->line);
+#else
+  fprintf(
+    ev->udata, "%s %-5s ",
+    buf, level_strings[ev->level]);
+#endif
 #endif
   vfprintf(ev->udata, ev->fmt, ev->ap);
   fprintf(ev->udata, "\n");
